9e3ceebff7863de78e31d44eba84c9cdffdc5f4e
Priyanshu Chaurasia <priyanshudlw1@gmail.com>
2026-01-05 17:10:06 +0530

    Add program to print an inverted triangle pattern based on user input

---
+++ CPP/04_PatternPrinting/triangleUlta.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m = 0 ;
+    cout<<"Enter m : ";
+    cin>>m;
+    for(int i=1; i<=m; i++){
+        
+        for(int j=1; j<=m-i+1; j++){
+            cout<<"*";
+           
+        }
+        cout<<endl;
+    }
+}
