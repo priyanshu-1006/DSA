33f5826031979f0b4d7546fb843ab0bb52053d68
Priyanshu Chaurasia <priyanshudlw1@gmail.com>
2026-01-05 16:59:49 +0530

    Add program to print the first n odd numbers

---
+++ CPP/03_Loops/AP.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 1;
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        cout<<1+2*i<<endl;
+    }
+}
