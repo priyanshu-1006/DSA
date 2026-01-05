20d97821e4e8228c011e756976fb809332ac3f32
Priyanshu Chaurasia <priyanshudlw1@gmail.com>
2026-01-05 17:09:08 +0530

    Add program to print a number square based on user input

---
+++ CPP/04_PatternPrinting/numberSquare.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m = 0 ;
+    cout<<"Enter m : ";
+    cin>>m;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=m; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
