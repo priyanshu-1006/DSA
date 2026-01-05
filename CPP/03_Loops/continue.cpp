a2e0348de99aa137fa7a08e8e69b50d1bad9bd59
Priyanshu Chaurasia <priyanshudlw1@gmail.com>
2026-01-05 17:02:47 +0530

    Add program to demonstrate the use of continue statement in loops

---
+++ CPP/03_Loops/continue.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){  
+    for(int i=1; i<=20; i++){
+        if(i==3 or i==8){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+
+}
