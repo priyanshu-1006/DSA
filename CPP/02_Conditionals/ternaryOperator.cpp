8a1cc3928af64aacbf8a2eb63f1518cefb6da33d
Priyanshu Chaurasia <priyanshudlw1@gmail.com>
2026-01-05 16:57:26 +0530

    Add program to determine if a number is even or odd using ternary operator

---
+++ CPP/02_Conditionals/ternaryOperator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    // (condition) ? if true : if false ;
+    (x%2==0) ? cout<<"Even" : cout<<"Odd"; 
+}
