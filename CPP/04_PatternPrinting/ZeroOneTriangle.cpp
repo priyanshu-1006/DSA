5ea8a5d35c24f4b654218ad90e2ceb7611e4568d
Priyanshu Chaurasia <priyanshudlw1@gmail.com>
2026-01-05 17:10:22 +0530

    Add program to print a zero-one triangle pattern based on user input

---
+++ CPP/04_PatternPrinting/ZeroOneTriangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m = 0 ;
+    cout<<"Enter m : ";
+    cin>>m;
+    for(int i=1; i<=m; i++){
+        
+        for(int j=1; j<=i; j++){
+            if((i+j)%2==0)cout<<1<<" ";
+            else cout<<0<<" ";
+           
+        }
+        cout<<endl;
+    }
+}
